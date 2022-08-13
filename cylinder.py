9a5f7da1222802b9bcc0bcede98158a5c0a41e3a
FATEMEHRAHIMIAN <110339993+FATEMEHRAHIMIAN@users.noreply.github.com>
2022-08-13 22:12:17 +0430

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,21 @@
+
+print('Enter your Height..')
+Height = int(input())
+
+
+print('Enter your Radius..')
+Radius = int(input())
+
+Side_area = (float((2*3.14)*Radius*Height))
+print("side area is ", float(Side_area))
+
+print("***************************************************")
+
+Volume = (float((3.14)*Radius*Radius*Height))
+print("Volume is ", float(Volume))
+
+print("***************************************************")
+
+total_area = (float((2*3.14)*Radius*(Radius+Height)))
+print("total area is ", float(total_area))
+
